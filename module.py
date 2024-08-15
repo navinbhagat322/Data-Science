47ef203f99b817c026548c45d30eb00c736553fe
navinbhagat322 <142095336+navinbhagat322@users.noreply.github.com>
2024-08-15 13:22:51 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def addition(n1,n2):
+    return n1+n2
+
+
+def substraction(n1,n2):
+    return n1-n2
+
+
+def multiplication(n1,n2):
+    return n1*n2
+
+
+def division(n1,n2):
+    return n1/n2
+
+
+# In[ ]:
+
+
+
+
